4cb307ed6ff3a864647cb1b71f14af678a3a7442
Gabreu <senra.gabreusoares@gmail.com>
2025-08-07 18:35:10 -0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
-    plugins: [ react()],
-    base: "/deploy-vite/", 
-  })
-/*https://vitejs.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -22,4 +19,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));*/
+  base: "/deploy-vite/", 
+}));
